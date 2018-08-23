d718145498f3881aec2aadb8eb9f468bae704abe
xiayong <xiayong@medto.cn>
2018-08-23 11:19:53 +0800

    springboot demo

---
+++ springboot/src/main/java/com/xiayong/springboot/App.java
@@ -0,0 +1,13 @@
+package com.xiayong.springboot;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
